60e8a46765a9e54cd8bf06f8282a1e0763fea38e
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-08-13 03:55:07 +0430

    Set left of 3part with JS

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Script document !
+$(function() {
+
+//Set left
+var sld = $('div.train > div');
+
+(function(){
+	for(i = 0; i<3 ;i ++){
+		sld.eq(i).css({left:i*320+'px'});
+	};
+})()
+
+
+});
