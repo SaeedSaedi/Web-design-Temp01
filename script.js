c11977088dbba0d24c681fc9f9b4cc64265b40bb
cyrus-krc <koorosh.krc@gmail.com>
2012-08-18 23:09:19 +0430

    again batman beginning :D

---
+++ script.js
@@ -0,0 +1,64 @@
+// Script document !
+$(function() {
+
+//Set left
+var sld = $('div.train > div');
+
+(function(){
+	for(i = 0; i<3 ;i ++){
+		sld.eq(i).css({left:i*320+'px'});
+	};
+	sld.css({backgroundImage:"url('images/first-background.jpg')"});
+	sld.eq(0).css({backgroundPosition:'10% 100%'});
+	sld.eq(1).css({backgroundPosition:'47% 100%'});
+	sld.eq(2).css({backgroundPosition:'83% 100%'});
+;})()
+
+//Hide|show black window on mouse over
+var opa = $('div.slide div.opa')
+sld.mouseover(function(){
+	clearop($(this).index());
+});
+sld.mouseout(function(){
+	showop($(this).index());
+});
+clearop = function(n){
+	opa.eq(n).animate({minHeight:'0'},500);
+		
+};
+showop = function(n){
+	opa.eq(n).animate({minHeight:'inherit'},500);
+};
+
+// Set click function
+var lock = false;
+sld.click(function(){
+	if(lock != false){
+		opn($(this).index());
+	}
+	else{
+		lck($(this).index());
+	}
+	
+});
+lck = function(n){
+	opa.eq(n).css({display:'none'});
+	lock = true;
+};
+opn = function(n){
+	opa.eq(n).css({display:'block'});
+	lock = false;
+};
+
+//Set slideshow for each sld
+sld1 = function(n){
+	sld.eq(0).animate({top:n*-680+'px'});
+};
+sld2 = function(n){
+	sld.eq(1).animate({top:n*-680+'px'});
+};
+sld3 = function(n){
+	sld.eq(2).animate({top:n*-680+'px'});
+};
+
+});
