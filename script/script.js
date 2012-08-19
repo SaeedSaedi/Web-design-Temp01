41c513baf639caa9285c0f71d4db97caf573b748
cyrus-krc <koorosh.krc@gmail.com>
2012-08-19 17:22:05 +0430

    add bot image

--- script/script.js
+++ script/script.js
@@ -8,10 +8,10 @@ var sld = $('div.train > div');
 	for(i = 0; i<3 ;i ++){
 		sld.eq(i).css({left:i*320+'px'});
 	};
-	sld.css({backgroundImage:"url('images/first-background.jpg')"});
-	sld.eq(0).css({backgroundPosition:'11.9% 100%'});
-	sld.eq(1).css({backgroundPosition:'48.2% 100%'});
-	sld.eq(2).css({backgroundPosition:'84.2% 100%'});
+	sld.css({backgroundImage:"url('images/final-background.jpg')",backgroundRepeat:'no-repeat'});
+	sld.eq(0).css({backgroundPosition:'11.5% 100%'});
+	sld.eq(1).css({backgroundPosition:'47.6% 100%'});
+	sld.eq(2).css({backgroundPosition:'83.8% 100%'});
 ;})()
 
 //Hide|show black window on mouse over
